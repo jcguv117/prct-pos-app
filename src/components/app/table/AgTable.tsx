0c90ecbd94052836e96ea7263590cfce54f54531
Carlos Guevara <jgv@datalabmx.com>
2024-12-20 18:34:20 -0600

    add handleShow list products

--- src/components/app/table/AgTable.tsx
+++ src/components/app/table/AgTable.tsx
@@ -111,12 +111,29 @@ export const AgTable = () => {
         }
       })
     }
+
+    const handleShow = (id: number) => {
+      const order = getOrderById(id);
+      let strItemsOrder = '';
+      order?.listItems.forEach( item => {
+        strItemsOrder += `<br> ${item.name} | $${item.price} x ${item.quantity} `
+      })
+      Swal.fire({
+        html: '<h2 class="mb-2 font-bold text-2xl">Lista de productos</h2> <hr/>' + strItemsOrder,
+        confirmButtonText: 'Aceptar',
+        customClass: {
+          popup: 'bg-white rounded-lg shadow-lg',
+          confirmButton: 'bg-blue-500 text-white rounded m-1 px-4 py-2 font-bold',
+        },
+        buttonsStyling: false,
+      })
+    }
     
 
     const actionButtoms = (id: number) => {
       return (
         <div class="flex flex-row gap-1">
-          <button class="text-stone-600" onClick={() => window.alert('mostrar') }><FontAwesomeIcon icon={faEye} size='xl' /></button>
+          <button class="text-stone-600" onClick={() => handleShow(id) }><FontAwesomeIcon icon={faEye} size='xl' /></button>
           <button class="text-amber-600" onClick={() => handleUpdateOrder(id)}><FontAwesomeIcon icon={faEdit} size='xl' /></button>
           <button class="text-green-600" onClick={() => handleConfirm(id)}><FontAwesomeIcon icon={faCircleCheck} size='xl' /></button>
           <button class="text-red-600"  onClick={() => handleCancel(id)}><FontAwesomeIcon icon={faCircleXmark} size='xl' /></button>
